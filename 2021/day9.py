85ed1d93c678bf624775ebb587addf99d82e466b
John Petrangelo <>
2021-12-10 11:59:01 -0500

    Finished day 9 part 2

--- 2021/day9.py
+++ 2021/day9.py
@@ -42,17 +42,56 @@ def part1():
                 total += center + 1
                 low_points.add((row, col))
     print(f"Part 1: Sum of risk levels of low points = {total}")
+    return low_points
 
 
-def part2():
-    print("Part 2: not yet written")
+def part2(low_points):
+    # List of basins we've discovered so far
+    basins = []
 
+    for low_point in low_points:
+        # If this low point is already in a known basin, move on
+        if True in [low_point in basin for basin in basins]:
+            continue
+
+        # The set of points that are part of this basin, starts with only the low_point
+        basin = set()
+
+        # Queue of points we need to check neighbors for
+        queue = [low_point]
+
+        while queue:
+            point = queue.pop()
+            if point in basin:
+                # We already know about this point
+                continue
+
+            row, col = point
+            height = get(row, col)
+            if height >= 9:
+                # This point is not in any basin
+                continue
+
+            basin.add(point)
+            queue.extend([(row-1, col), (row+1, col), (row, col-1), (row, col+1)])
+
+        basins.append(basin)
+
+    sizes = []
+    for basin in basins:
+        sizes.append(len(basin))
+    sizes.sort(reverse=True)
+
+    result = sizes[0] * sizes[1] * sizes[2]
+    print(f"Part 2: Product of sizes of largest three basins is {result}")
+
+    return basins
 
 
 def main():
     read_input("input9.txt")
-    part1()
-    part2()
+    low_points = part1()
+    part2(low_points)
 
 
 if __name__ == '__main__':
