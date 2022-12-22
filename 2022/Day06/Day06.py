b39a50f8c58b94a93c8eb07afb3dbe0d1c8f542a
John-Petrangelo-Bose <john_petrangelo@bose.com>
2022-12-21 23:31:07 -0500

    Fixed day 6 commit

--- 2022/Day06/Day06.py
+++ 2022/Day06/Day06.py
@@ -1,97 +1,37 @@
-def read_file(filename):
-    stacks = [[] for _ in range(9)]
-    procedures = []
+example_data_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+example_data_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+example_data_3 = "nppdvjthqldpwncqszvftbrmjlhg"
+example_data_4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+example_data_5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
-    with open(filename) as f:
-        # Read the stacks
-        for line in f:
-            if line[1] == "1":
-                # print("Found column labels")
-                break
-            index = 0
-            while line:
-                # print(f"line={line.rstrip()}=")
-                letter = line[1]
-                if letter != ' ':
-                    # print(f"Adding {letter} to stack {index}")
-                    stacks[index].append(letter)
-                index += 1
-                line = line[4:]
-
-        # Flip all of the stacks, we read the top-to-bottom
-        for index in range(9):
-            stacks[index].reverse()
-
-        # Read the blank line separating stacks from procedures
-        _ = f.readline()
-
-        # Read the procedures
-        for line in f:
-            line = line.strip()
-            parts = line.split(" ")
-            procedures.append((int(parts[1]), int(parts[3]), int(parts[5])))
-        print(f"Found {len(procedures)} procedures")
-
-    return stacks, procedures
+puzzle_data = "srjsssgppnssqzszmzjmjdmmqwqcwqccslsjswjssgbsgsnggqtqnnjznndtndnhddfldfdsfswsjsjjptjpttlwlccpnpgngcncnscncsnnwrwmwggsgdgssvbsstzsstqqrjqqlsqlsqscschhclhccznzcnnzzqppjbpjjqzzbwbqqzdqdjjqtjtbtgtqgtgdgwwsjjbwjjzssrsvsttgqtgglgplgpllcrcncssbrbgrbrllsqlsltthshrrnddwzdzdbbrppdvdqvvpvdvdsslbbmnbnjjrdrnntzzftzftttrsshpspvvrzzqzwzhzszbzjjwqwvqqglgflfwfrfprpddfvdffhzztwtppwwwztzsznzmnnbvbbtqqdhqdqdfffnjnmnlmljmlmnlnddzbbdgdqdnqqtjjtnthttvwwtrwwwgffqcfchfcfwcczhhgjhghpgpwgwffhghzghhtftntnnmlnmlmddqbbfwbfwbwvvfssrggptpltpllbcbzczzlccjgcgvcczjzvvvdtvdvqdqwdqdvdjvddjfdjjqmmfgfsftfzfwzwzfftmfmmqdqccjqcqwwflwlccpssvzzzbnbjnbjjgtgpttbwbrbwrwvrrwccgrgrvvpjvvznvvhddtdldpldppmlplltdllhqhpqhqqbpptgpgjgqjgjffvfdvdttsrsllhzlzrrdrnrjrddqbbjrrvrsstgsttjqjhjbhhnmndmmnrmnnrggmtgmmhshjjfqjjtqtstpstppggtzzrsrwwqffvzvzvqqggqsqtstszttpvtvvvddcncvncvchhnsnpsnnlmnnmqnqjjfwfccmwmbwbswbswwghhzrrptptbbjbtjjgdjgjrgrwgwlglblbnlnbbwnnpbnbmbtmtvvhjjlwlhhszslldwlwdllhjhghqghhqpplhhtjhhnhqhlhjhmmlhlmmrpptvvcczfcccwgcccpgpspdpccfhchffbjbzzwppfbbstbtltpllcssnnctnnqcqhhclhccvlvlffnjjwbwfbfttwvvdvnnnsrstrtmtfthtzzcvzvhzzpmpfpmmmwggdbdqdbbjnbbdqbbrnnnprrwbbcwbbpjjprjjzzvwwvrrthhqvhvnhhzmzszrszsjssclcjjbhjbhjbhjbjhhzbbzttnrnssrbrjbrjjmsmffdlflfzfbzzmtztdthdhldhlddnccgbbmtbbsbzsbzzcsszpzfffprfpfzfrzzhvhffsmmqtjwgjbzhnmrslrmgfjpqcllcgsjdhrshqtlgmqqtfmswfzwtnrswtzdjzclcfmltqgdhcsgvzrdltgfbtqclpppvbqnbqmlhmdbsjbwbdllzpnrwfhmnlgvdwsdjsznnhqzhwntjvcpzdrfwmwwdrttdvzspmbmqggmlmsvwgcjgpvcmplqwfjgpghnfpbctnfhcgngcbdmzhlpcnjpmczzsgfgrdftrzgvmmpdmgcrpcdrjsgczpfjnwpdjpntdngdjwctvcbsjmfwvtsrlhvpswppmfwrwzsbsgbjvljzqjjldmnqnmwsmqnmhmqhhttppbpqlvdcvdbhmbnjzztjrdjdzlvmbdrghtftwdpcwwblsjbgnzwtpztmtmnrpsvzfzncqmrvhcbqcvqvnlngdcllrqlbhjttnmjmhfhdzmjmplcfdqpmwblzsnmpczwcnggcwdvgnjcrrtmpwwqdqpvtbzpdbfnbfcfllntqjlslcsznjvzvsbntrtzwhcbtdmbmwttvhdvdtvrcmprcrrjlgsqddrsmwsrbtbpjrmlbrnsdrjfhjnqjtgjmhzjbjnprvmhtjcdbztwqmrlfflfmcslshtwmwhgvbdslgjhzjlglhllsdphlzngjfwfrlwpnqfhghnqhzhgrszbcwjvlrtmshntszsqplvfbccjwctgtfmqgqjdlgdbwhgvctqtcgfdwvqdwqzddmsbrpftzpqztgzbnplvhftmgpdthnrdhqbltbrmhpcqsfccmqzwmrbnbgbjspslwpjhdqspssqdtnssmjmvzwwfstgzzjrmfczdlznwqpdhbsjqddvffcgfhfdqdrlwcsgcsszdtpqbbsthpwbhdfzmgmcdggfcwcmzfjnfbzbccjhvwhbwfslnqnrwhgrwtlnmrmncnjtbjbdlmqsczppgbcmsdrwlrpjbgrmnhqqfhhsdhmdmpvvpjrnzsvzctmqhpzcvcjfgtlfvqvnvlnprmgrsvrrvtjfndqfsvqdsfbcwlbglmfhfhcfgqdfmclnzhdtppgzzsqgjqncqrbdhlhdjqwjpmbdnfmgdgwbwmnlngnmrhcgqwzvmbjzdvsspjwwdtpnvpftdlqlzfgtscfczsvbrtrqqpgqlvmrtddqplbzsswbgpdzpqfvqpqbndblghmdhmnctdnjbgglmrlvmrmsfgntfdwvqcvvlvbcnwrctvjqhmnsjqccwltbfqpqpmwsfvmnnfqmlmlcchqcdtbvqwcpptvfwrwtbdrlsgnwpmjgnwlprzqjlwqmtmjglbrzlgfbsghwqdmwhrcmfwdmzmflsbngtgndftdpzsqvgqdsfdhplmfcmwpbtvcdmpghmfwqjvhdhfpmbrqpvnbhlftgdtprlztrgnlcldfpjqjqdfrvqtcnzrtjcgzgsslzghlnfhwwjwzdsmpsczclrfmnqjvfmvsqpntsnnnlrfswqtrppzhqgjzlzvrrbhhfhchhvgztpgctcsgssvttszsrdwzwrbmwmspgqhmmfnzqqdbmnbltdmrsvqgddltwczbbjcmplncspgqgmzrndhttsrbvqbpbvhshfqrpqgmmdbhmmtccjcmntmpqhrvhnfnlqqbctsnfzjbphhqwmztgbhqqlbctbsfcszbggzrlcdhwddtjgtqhppzgjsqcddwjsngjrcdflmgwgfnzhjtcwgbqvpwmpgcpdwvqgswwfzcnjgmdpffmqczmsqgpthpmsjlwnrcbzrfshvwftzllwrmfccmlpjnmpjdfpcvjgjpznllmqjwpcflgqgdljtbbjvjlvhhmtvzfnjfnnwrvtlfdbhqphrjghtmlsrplqscsnvjvqdslsbsfzzrjfmchplzgjgdqvzhfphvsjfvnqlgmjfzhrdlmmvfntnzdvrnwqshsmtjnqmwzgpbbzszrsqcvlzjwnmgjhmfqrbvgmfqpswctmvpfcghvdqgstglmzvpfhzfzvhqqdmvrvrttlpwwhqgddzqlrvvdffqtznvlfgjhhmvbmtzjqnnhqzrtbzpqcwpngrdcndcgzwhzgtfwbmwbrpvvczczhwcqwsqzqbqvqftcswtcbzdbpccjhtwbpnwlwwwqwscptlwshrdbmmdcgrmpnnwgjwzszwwdwctfspbfqvdjqtrflshrqlbfgpnrmbszwjpcdzbggphgplcgvwljprzmtncsvfwqchttndhpnzmtdvtjqtcsddtqvcmztmjgwqvjhflrjjtnvfpjrnlvzvwvrpbhrzslmrqqzqhnzqnvtqppmncddphbwwsjczmphsrlltzndtqjgdlqgpnlfcvwntstmrgcrjzmmllpwldnwmwzpvzctmhszspcvtgnqthszzsmtdnzwtfddfctpjhscbqgwfpqmzpvqrzvtbrdjzrqprdgbpmzzfbgqcvcdtsfrffcpqwtvdwvtcqlcsdrzntgrhrspznndslmnvptlphpdqgbblfhmgbpmmfwqzlhvzshhpzgfjldqclngbcbrmmnqqvqmwdnjsglsggqfgjldqfbsqgtrwmpdffqlcwwlfhlpqfgwtssnjwzhgvtwqzmhmgwzwmcggmpmrzqrcsmflqsrbnzvdmjcdbnscstqrqhvddsbjpzwsvzswqhcqmgzlvfcnzjrrffzphmrvdbhqbrwpsfqvfqwhqhcgfvfsfttzcdsrjgjwcgvhllszmplmvgczqsbfldnbvrnqccbprjjdwhmqpdjjrnfdlhzdvlfmrldjlqclbjrrtjfsflphzdcdpfpr "
 
 
-def part_1(filename):
-    print(f"========================================")
-    print(f"Running part 1 with file {filename}")
+def run(data, marker_len):
+    print(f"Using data {data[:10]}...")
 
-    stacks, procedures = read_file(filename)
-
-    for p in procedures:
-        print(f"Procedure: {p}")
-        count, from_stack, to_stack = p
-        print(f"BEFORE from:{stacks[from_stack-1]}  to:{stacks[to_stack-1]}")
-        for _ in range(count):
-            stacks[to_stack-1].append(stacks[from_stack-1].pop())
-        print(f"AFTER  from:{stacks[from_stack - 1]}  to:{stacks[to_stack - 1]}")
-
-    answer = ""
-    for index in range(9):
-        if stacks[index]:
-            print(f"Top of stack {index} is {stacks[index][-1]}")
-            answer += stacks[index][-1]
-        else:
-            print(f"Stack {index} is empty")
-            answer += " "
-    print(f"Stack tops are {answer}")
+    for i in range(marker_len-1, len(data)):
+        chars = set()
+        for j in range(i-marker_len+1, i+1):
+            chars.add(data[j])
+        if len(chars) == marker_len:
+            print(f"Must process {i+1} characters")
+            return
 
 
-def part_2(filename):
-    print(f"========================================")
-    print(f"Running part 2 with file {filename}")
+print(f"========================================")
+print(f"Running part 1")
+run(example_data_1, 4)
+run(example_data_2, 4)
+run(example_data_3, 4)
+run(example_data_4, 4)
+run(example_data_5, 4)
+run(puzzle_data, 4)
 
-    stacks, procedures = read_file(filename)
-
-    for p in procedures:
-        print(f"Procedure: {p}")
-        count, from_stack, to_stack = p
-        print(f"BEFORE from:{stacks[from_stack-1]}  to:{stacks[to_stack-1]}")
-        crates = []
-        for _ in range(count):
-            crates.append(stacks[from_stack-1].pop())
-        crates.reverse()
-        stacks[to_stack - 1].extend(crates)
-        print(f"AFTER  from:{stacks[from_stack - 1]}  to:{stacks[to_stack - 1]}")
-
-    answer = ""
-    for index in range(9):
-        if stacks[index]:
-            print(f"Top of stack {index} is {stacks[index][-1]}")
-            answer += stacks[index][-1]
-        else:
-            print(f"Stack {index} is empty")
-            answer += " "
-    print(f"Stack tops are {answer}")
-
-
-part_1("Day05_example.txt")
-part_1("Day05.txt")
-
-part_2("Day05_example.txt")
-part_2("Day05.txt")
-
-
+print(f"Running part 2")
+run(example_data_1, 14)
+run(example_data_2, 14)
+run(example_data_3, 14)
+run(example_data_4, 14)
+run(example_data_5, 14)
+run(puzzle_data, 14)
